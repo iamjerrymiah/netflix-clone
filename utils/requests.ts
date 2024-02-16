c3f2b6e1d517bf93be71d4755ff1a709f64618ef
unknown <jnwafor@techmadeeazy.com>
2024-02-16 23:21:45 +0100

    second commit

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,4 +1,5 @@
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+// const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+const API_KEY = 'ff18ea51a9fbc5f1fe482df3d81d2027'
 const BASE_URL = 'https://api.themoviedb.org/3'
 
 const requests = {
